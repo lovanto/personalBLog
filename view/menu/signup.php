2f36d49709c18b8e53c75d1321d5ebfc923bbb40
lovanto <rifkylovanto@gmail.com>
2019-12-28 21:37:45 +0700

    sign up view created

--- view/menu/signup.php
+++ view/menu/signup.php
@@ -1,1 +1,31 @@
-ini signup
+<form action="" method="POST">
+	<div class="container">
+		<div class="boldFont">Buat Akun Baru</div>
+		<hr>
+		<div class="input-group marginsBottom col-md-6">
+			<div class="input-group-prepend">
+				<div class="input-group-text">Nama Lengkap</div>
+			</div>
+			<input class="form-control" type="text" name="name_user">
+		</div>
+		<div class="input-group marginsBottom col-md-5">
+			<div class="input-group-prepend">
+				<div class="input-group-text">Username</div>
+			</div>
+			<input class="form-control" type="text" name="username_user">
+		</div>
+		<div class="input-group marginsBottom col-md-7">
+			<div class="input-group-prepend">
+				<div class="input-group-text">Email</div>
+			</div>
+			<input class="form-control" type="email" name="email_user">
+		</div>
+		<div class="input-group marginsBottom col-md-5">
+			<div class="input-group-prepend">
+				<div class="input-group-text">Password</div>
+			</div>
+			<input class="form-control" type="password" name="password_user">
+		</div>
+		<div align="center"><input class="btn btn-primary col-md-2" type="submit" name="submit" value="Daftar"></div>
+	</div>
+</form>
