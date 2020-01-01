262483b92f56d26cf4d204a801d69e7e0d22cef9
lovanto <rifkylovanto@gmail.com>
2020-01-01 23:22:47 +0700

    css moved

--- view/adminSide/bodyAdmin.php
+++ view/adminSide/bodyAdmin.php
@@ -16,15 +16,15 @@ $countComment = mysqli_num_rows($resultComment);
 	<div class="bg-light">
 		<div class="p-5">
 			<div class="jumbotron p-3 boldText mb-3">
-				<img class="mr-2" src="../../images/icon/home.png" width="20" style="margin-top: -4px;"> Dashboard
+				<img class="mr-2 minMT" src="../../images/icon/home.png" width="20"> Dashboard
 			</div>
 			<div class="alert alert-success" role="alert">
 				Selamat datang <?=$_SESSION['name']?> di dashboard admin...
 			</div>
 			<div class="row">
-				<div class="card col-sm m-3 zoom" style="width: 18rem;">
+				<div class="card col-sm m-3 zoom widthCardTop">
 					<ul class="list-group list-group-flush">
-						<div class="bg-primary rounded-top p-3 whiteFont boldFont row" style="margin-left: -16px; margin-right: -16px;">
+						<div class="bg-primary rounded-top p-3 whiteFont boldFont row minRNL">
 							<div class="col-sm">
 								<img class="mr-2" src="../../images/icon/user.png" width="120">
 							</div>
@@ -33,14 +33,14 @@ $countComment = mysqli_num_rows($resultComment);
 						<li class="list-group-item" align="right">
 							<a class="link_item" href="">
 								Klik untuk lebih lengkapnya..
-								<img class="ml-2" src="../../images/icon/plus.png" width="20" style="margin-top: -4px;">
+								<img class="ml-2 minMT" src="../../images/icon/plus.png" width="20">
 							</a>
 						</li>
 					</ul>
 				</div>
-				<div class="card col-sm m-3 zoom" style="width: 18rem;">
+				<div class="card col-sm m-3 zoom">
 					<ul class="list-group list-group-flush">
-						<div class="bg-success rounded-top p-3 whiteFont boldFont row" style="margin-left: -16px; margin-right: -16px;">
+						<div class="bg-success rounded-top p-3 whiteFont boldFont row minRNL">
 							<div class="col-sm">
 								<img class="mr-2" src="../../images/icon/post.png" width="120">
 							</div>
@@ -49,14 +49,14 @@ $countComment = mysqli_num_rows($resultComment);
 						<li class="list-group-item" align="right">
 							<a class="link_item" href="">
 								Klik untuk lebih lengkapnya..
-								<img class="ml-2" src="../../images/icon/plus.png" width="20" style="margin-top: -4px;">
+								<img class="ml-2 minMT" src="../../images/icon/plus.png" width="20">
 							</a>
 						</li>
 					</ul>
 				</div>
-				<div class="card col-sm m-3 zoom" style="width: 18rem;">
+				<div class="card col-sm m-3 zoom">
 					<ul class="list-group list-group-flush">
-						<div class="bg-danger rounded-top p-3 whiteFont boldFont row" style="margin-left: -16px; margin-right: -16px;">
+						<div class="bg-danger rounded-top p-3 whiteFont boldFont row minRNL">
 							<div class="col-sm">
 								<img class="mr-2" src="../../images/icon/comment.png" width="120">
 							</div>
@@ -65,7 +65,7 @@ $countComment = mysqli_num_rows($resultComment);
 						<li class="list-group-item" align="right">
 							<a class="link_item" href="">
 								Klik untuk lebih lengkapnya..
-								<img class="ml-2" src="../../images/icon/plus.png" width="20" style="margin-top: -4px;">
+								<img class="ml-2 minMT" src="../../images/icon/plus.png" width="20">
 							</a>
 						</li>
 					</ul>
