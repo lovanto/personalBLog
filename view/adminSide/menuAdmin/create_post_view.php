955494cfe7bf72ef0a6976d15e5251e9a38516f8
lovanto <rifkylovanto@gmail.com>
2020-01-06 22:00:52 +0700

    add post view v3

--- view/adminSide/menuAdmin/create_post_view.php
+++ view/adminSide/menuAdmin/create_post_view.php
@@ -3,9 +3,31 @@
 <!-- <form action="../../code/update/update_user.php" method="POST"> -->
 	<div class="row">
 		<div class="col-sm border-right">
-			<div class="marginsBottom">
-				<button class="btn btn-default" name="btn-compose" id="btn-compose">Compose</button>
-				<button class="btn btn-default" name="btn-html" id="btn-html">HTML</button>
+			<div class="row container">
+				<div class="marginsBottom border mr-2">
+					<button class="btn btn-default m-1 border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Compose mode">Compose</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="HTML mode">HTML</button>
+				</div>
+				<div class="marginsBottom border mr-2">
+					<button class="btn btn-default m-1 boldFont border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Bold">B</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="Italic" style="font-style: italic;">I</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Underline" style="text-decoration: underline;">U</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="Strikethrough" style="text-decoration: line-through;">ABC</button>
+				</div>
+				<div class="marginsBottom border mr-2">
+					<button class="btn btn-default m-1 boldFont border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Text color">Text Color</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="Text background color" style="font-style: italic;">Text Background Color</button>
+				</div>
+				<div class="marginsBottom border mr-2">
+					<button class="btn btn-default m-1 boldFont border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Insert link">Link</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="Insert image" style="font-style: italic;">Img</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Insert video" style="text-decoration: underline; ">Video</button>
+				</div>
+				<div class="marginsBottom border mr-2">
+					<button class="btn btn-default m-1 boldFont border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Align">Align</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-html" id="btn-html" title="Numeric list" style="font-style: italic;">Numeric</button>
+					<button class="btn btn-default mr-1 mt-1 mb-1 border border-rounded zoomButton minFont" name="btn-compose" id="btn-compose" title="Bullet list" style="text-decoration: underline; ">Bullet</button>
+				</div>
 			</div>
 			<div>
 				<div class="input-group-text mb-1">Konten Postingan</div>
@@ -48,34 +70,34 @@
 			</div>
 		</div>
 	</div>
-<!-- </form> -->
+	<!-- </form> -->
 
-<script>
-	$(document).ready(function() {
-		document.getElementById("btn-compose").classList.add('activeButton'); 
-		$("#btn-compose").click(showCompose);
-		$("#btn-html").click(showHtml);
-	});
+	<script>
+		$(document).ready(function() {
+			document.getElementById("btn-compose").classList.add('activeButton'); 
+			$("#btn-compose").click(showCompose);
+			$("#btn-html").click(showHtml);
+		});
 
-	$("#image_post").change(function() {
-		filename = this.files[0].name;
-		$("#label-image").text(filename);
-	});
+		$("#image_post").change(function() {
+			filename = this.files[0].name;
+			$("#label-image").text(filename);
+		});
 
-	function showCompose() {
-		document.getElementById("comment_content").classList.add('visible-true'); 
-		document.getElementById("comment_content_html").classList.add('visible-false');  
-		document.getElementById("comment_content_html").classList.remove('visible-true'); 
-		document.getElementById("btn-compose").classList.add('activeButton'); 
-		document.getElementById("btn-html").classList.remove('activeButton'); 
-	}
+		function showCompose() {
+			document.getElementById("comment_content").classList.add('visible-true'); 
+			document.getElementById("comment_content_html").classList.add('visible-false');  
+			document.getElementById("comment_content_html").classList.remove('visible-true'); 
+			document.getElementById("btn-compose").classList.add('activeButton'); 
+			document.getElementById("btn-html").classList.remove('activeButton'); 
+		}
 
-	function showHtml() {
-		document.getElementById("comment_content_html").classList.add('visible-true'); 
-		document.getElementById("comment_content").classList.add('visible-false'); 
-		document.getElementById("comment_content").classList.remove('visible-true'); 
-		document.getElementById("btn-html").classList.add('activeButton'); 
-		document.getElementById("btn-compose").classList.remove('activeButton'); 
-	}
+		function showHtml() {
+			document.getElementById("comment_content_html").classList.add('visible-true'); 
+			document.getElementById("comment_content").classList.add('visible-false'); 
+			document.getElementById("comment_content").classList.remove('visible-true'); 
+			document.getElementById("btn-html").classList.add('activeButton'); 
+			document.getElementById("btn-compose").classList.remove('activeButton'); 
+		}
 
-</script>
+	</script>
