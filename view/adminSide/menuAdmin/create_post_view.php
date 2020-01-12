dccfcfabc4fd94ce367c854e471d74aa93628921
lovanto <rifkylovanto@gmail.com>
2020-01-12 15:41:07 +0700

    google drive deleted and cloudinary added

--- view/adminSide/menuAdmin/create_post_view.php
+++ view/adminSide/menuAdmin/create_post_view.php
@@ -4,18 +4,18 @@
 <link rel="stylesheet" href="../../ckeditor/samples/toolbarconfigurator/lib/codemirror/neo.css">
 
 <h1>Create new post</h1>
+<a href="#uploadImage">Upload</a>
 <hr>
 <form action="../../code/create/create_post.php" method="POST">
-	<input class="form-control" type="hidden" name="id_user" id="id_user" value="<?=$_SESSION['id_user'];?>" required>
-	<input class="form-control" type="hidden" name="name" id="name" value="<?=$_SESSION['name'];?>" required>
+	<input class="form-control" type="hidden" name="id_user" id="id_user" value="<?=$_SESSION['id_user'];?>">
+	<input class="form-control" type="hidden" name="name" id="name" value="<?=$_SESSION['name'];?>">
 	<div class="row">
-
 		<div class="col-sm minML-2 minMR-2">
 			<main>
 				<div class="adjoined-bottom">
 					<div class="grid-container">
 						<div class="grid-width-100">
-							<textarea id="editor" name="editor" rows="10000" required>
+							<textarea id="editor" name="editor" rows="10000">
 								<h1>Hello world!</h1>
 								<p>I'm an instance of <a href="https://ckeditor.com">CKEditor</a>.</p>
 							</textarea>
@@ -29,31 +29,24 @@
 				<div class="input-group-prepend">
 					<div class="input-group-text">Judul</div>
 				</div>
-				<?php include '../../googledrive/index.php';?>
-			</div>
-
-			<div class="input-group marginsBottom">
-				<div class="input-group-prepend">
-					<div class="input-group-text">Judul</div>
-				</div>
-				<input class="form-control" type="text" name="title_post" id="title_post" maxlength="70" required>
+				<input class="form-control" type="text" name="title_post" id="title_post" maxlength="70">
 			</div>
 
 			<div class="custom-file marginsBottom">
 				<label class="custom-file-label" id="label-image" for="image_post">Pilih foto</label>
-				<input type="file" class="custom-file-input" id="image_post" name="image_post" required>
+				<input type="file" class="custom-file-input" id="image_post" name="image_post">
 			</div>
 
 			<div class="marginsBottom">
 				<div class="input-group-text mb-1">Deskripsi Postingan</div>
-				<textarea class="form-control textUnResize" name="description_post" id="description_post" maxlength="245" rows="3" required></textarea>
+				<textarea class="form-control textUnResize" name="description_post" id="description_post" maxlength="245" rows="3"></textarea>
 			</div>
 
 			<div class="input-group marginsBottom">
 				<div class="input-group-prepend">
 					<div class="input-group-text">Kategori</div>
 				</div>
-				<select class="form-control" name="id_category" id="id_category" required>
+				<select class="form-control" name="id_category" id="id_category">
 					<option value="1">Programming</option>
 					<option value="2">Tutorial</option>
 					<option value="3">Emulator</option>
@@ -71,3 +64,8 @@
 <script>
 	initSample();
 </script>
+
+<!-- Login -->
+<div id="uploadImage" class="modal-window">
+	<?php include '../../cloudinary/index.php'; ?>
+</div>
