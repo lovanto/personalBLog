d649cbb040331b13041b3d0e13ea5d8c1c17048d
lovanto <rifkylovanto@gmail.com>
2020-01-13 21:50:39 +0700

    ajwdnawjdlnaw 4

--- index.php
+++ index.php
@@ -19,15 +19,11 @@ session_start();
 <body>
 
 	<?php
-	require './vendor/autoload.php';
-	require './cloudinary/vendor/autoload.php';
-	require './cloudinary/config.php';
-
-	include './conn.php';
-	include './code/clock.php';
-	include './view/header.php';
-	include './view/body.php';
-	include './view/footer.php';
+	include 'conn.php';
+	include 'code/clock.php';
+	include 'view/header.php';
+	include 'view/body.php';
+	include 'view/footer.php';
 	?>
 </body>
 </html>
